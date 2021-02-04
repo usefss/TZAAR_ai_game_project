b7d0f3e33b086c9461a8708fd9976c7a6d7889e5
yosef salmalian <flaslnm@gmail.com>
2021-02-04 08:11:52 +0330

    ga fitness

--- src/GaFitness.java
+++ src/GaFitness.java
@@ -1,9 +1,20 @@
-public class GaFitness {
-    public GaFitness() {
+import models.Board;
+import models.Game;
 
+public class GaFitness {
+
+    private Board start_state = null;
+    private Board end_state = null;
+
+
+    public GaFitness() {}
+
+    private void set_states() {
+        
     }
 
     public int fitness() {
+        set_states()
 
         return 0;
     }
--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,63 @@
 import models.Game;
 import models.Player;
 import models.PlayerType;
+import models.Board;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 public class Main {
 
-    public static void main(String[] args) {
-        // RandomPlayer whitePlayer = new RandomPlayer(PlayerType.white);
+    public static void test_file() {
         WarriorZ whitePlayer = new WarriorZ(PlayerType.white);
-        // Ai whitePlayer = new Ai(PlayerType.white);
-        // OptimizedAI blackPlayer = new OptimizedAI(PlayerType.black);
         AiDeep blackPlayer = new AiDeep(PlayerType.black);
         Game game = new Game(whitePlayer, blackPlayer);
+
+        Board board = game.getBoard();
+        board.printComplete();
+        
+        try {
+            // write object to file
+            FileOutputStream fos = new FileOutputStream("start.ser");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(board);
+            oos.close();
+    
+            // read object from file
+            FileInputStream fis = new FileInputStream("start.ser");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            // MyBean result = (MyBean) ois.readObject();
+            Board result = (Board) ois.readObject();
+            ois.close();
+
+            result.printComplete();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+    }
+
+    public static void start_game() {
+
+        WarriorZ whitePlayer = new WarriorZ(PlayerType.white);
+        AiDeep blackPlayer = new AiDeep(PlayerType.black);
+        Game game = new Game(whitePlayer, blackPlayer);
+
         Player player = game.play();
+
         System.out.println(player.getType());
+
+    }
+    public static void main(String[] args) {
+        // start_game();
+        test_file();
     }
 
 }
